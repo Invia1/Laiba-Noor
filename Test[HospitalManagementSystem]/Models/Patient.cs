3ce8b98fa03d499c0c76e47e917ad9ab116df6d5
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-18 08:56:34 +0530

    Update Patient.cs

--- Test[HospitalManagementSystem]/Models/Patient.cs
+++ Test[HospitalManagementSystem]/Models/Patient.cs
@@ -13,33 +13,7 @@ namespace HospitalManagementSystem.Models
         public int Age { get; set; }
         public string Address { get; set; }
         public uint AadharNo { get; set; }
-        public int SpecialityToBeConsulted { get; set; }
-
-        //readonly int PatientRegId;
-        //string FirstName;
-        //string LastName;
-        //uint PhoneNo;
-        //string Gender;
-        //int Age;
-        //string Address;
-        //readonly uint AadharNo;
-        //string SpecialityToBeConsulted;
-        //const string Speciality1 = "General medicine";
-        //const string Speciality2 = "Orthopaedics";
-        //const string Speciality3 = "Dental";
-
-        //public Patient(int patientRegId, string firstName, string lastName, uint phoneNo, string gender, int age, string address, uint aadharNo, string specialityToBeConsulted)
-        //{
-        //    PatientRegId = patientRegId;
-        //    FirstName = firstName;
-        //    LastName = lastName;
-        //    PhoneNo = phoneNo;
-        //    Gender = gender;
-        //    Age = age;
-        //    Address = address;
-        //    AadharNo = aadharNo;
-        //    SpecialityToBeConsulted = specialityToBeConsulted;
-        //}
+        public string SpecialityToBeConsulted { get; set; }
 
     }
 }
